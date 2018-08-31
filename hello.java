e04cffbaeae858b9c591cabeef9ecd1a94a46dd5
yaochenzhi <yaochenzhi@localhost.localdomain>
2018-08-31 23:44:54 +0800

    Hello world!

---
+++ hello.java
@@ -0,0 +1,9 @@
+class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println();
+        System.out.println("Steps:");
+        System.out.println("\tjavac hello.java");
+        System.out.println("\tjava helloworld");
+    }
+}
