ebc65fc6384d7167eabe562415f5c4275ae64c2b
yaochenzhi <yaochenzhi@localhost.localdomain>
2018-09-01 19:49:43 +0800

    Constructor in Java

--- call_print.java
+++ call_print.java
@@ -4,5 +4,19 @@ class call_print {
         printer.print_hello();
 
         print_hello.static_print_hello();
+
+
+        String[] fullname2 = {"Jack", "Ma"};
+        
+
+        String[] fullname = new String[2];
+        fullname = new String[] {"Jack", "Ma"};
+
+
+        String fullname3[] = new String[2];
+        fullname3[0] = "Jack";
+        fullname3[1] = "Ma";
+
+        print_hello_with_args printer_with_args = new print_hello_with_args(fullname);
     }
 }
--- print_hello.java
+++ print_hello.java
@@ -7,3 +7,10 @@ class print_hello {
         System.out.println("Hello world from static_print_hello method in print_hello.java.");
     }
 }
+
+class print_hello_with_args {
+    public print_hello_with_args(String[] args){
+        String name = args[0];
+        System.out.println("Hello " + name);
+    }
+}
